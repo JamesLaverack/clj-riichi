25d8534cc39e93e05929c928ee41988b9540905a
James Laverack <james@jameslaverack.com>
2017-06-20 22:59:26 +0100

    Add terminal testing code

--- src/mahjong/core.clj
+++ src/mahjong/core.clj
@@ -6,6 +6,15 @@
   [tile]
   (not (contains? tile :suit)))
 
+(defn suit?
+  "Is the given tile a suit tile?"
+  [tile]
+  (not (honor? tile)))
+
+(defn terminal?
+  [tile]
+  (and (suit? tile) (or (= (:number tile) 1) (= (:number tile) 9))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/mahjong/core_test.clj
+++ test/mahjong/core_test.clj
@@ -2,16 +2,31 @@
   (:require [clojure.test :refer :all]
             [mahjong.core :refer :all]))
 
-(def seven-of-bamboo #{:suit :bamboo :number 7})
-(def green-dragon #{:dragon :green})
-(def north-wind #{:wind :north})
+(def seven-of-bamboo {:suit :bamboo :number 7})
+(def nine-of-characters {:suit :characters :number 9})
+(def one-of-coins {:suit :coins :number 1})
+(def green-dragon {:dragon :green})
+(def north-wind {:wind :north})
 
 
-(deftest a-test
+(deftest honor-test
   (testing "That 7 Bamboo is not an honor"
     (is (not (honor? seven-of-bamboo))))
   (testing "That the Green Dragon is an honor"
     (is (honor? green-dragon)))
   (testing "That the North Wind is an honor"
     (is (honor? north-wind)))
+  )
+
+(deftest terminal-test
+  (testing "That 7 Bamboo is not a terminal"
+    (is (not (terminal? seven-of-bamboo))))
+  (testing "That the Green Dragon is not a terminal"
+    (is (not (terminal? green-dragon))))
+  (testing "That the North Wind is not a terminal"
+    (is (not (terminal? north-wind))))
+  (testing "That 9 Characters is a terminal"
+    (is (terminal? nine-of-characters)))
+  (testing "That 1 Coins is a terminal"
+    (is (terminal? one-of-coins)))
   )
