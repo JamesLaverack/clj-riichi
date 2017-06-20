0046763593047a94f1465ba78c8d833991bc79f3
James Laverack <james@jameslaverack.com>
2017-06-20 22:34:43 +0100

    Lein project boilerplate

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mahjong
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mahjong/core_test.clj
@@ -0,0 +1,7 @@
+(ns mahjong.core-test
+  (:require [clojure.test :refer :all]
+            [mahjong.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
