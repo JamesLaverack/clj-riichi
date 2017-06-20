23898541807af107f4c05f5cee1cc67a56df1b8b
James Laverack <james@jameslaverack.com>
2017-06-20 22:48:36 +0100

    Add honor testing code

--- src/mahjong/core.clj
+++ src/mahjong/core.clj
@@ -1,6 +1,11 @@
 (ns mahjong.core
   (:gen-class))
 
+(defn honor?
+  "Is the given tile an honor?"
+  [tile]
+  (not (contains? tile :suit)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/mahjong/core_test.clj
+++ test/mahjong/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [mahjong.core :refer :all]))
 
+(def seven-of-bamboo #{:suit :bamboo :number 7})
+(def green-dragon #{:dragon :green})
+(def north-wind #{:wind :north})
+
+
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "That 7 Bamboo is not an honor"
+    (is (not (honor? seven-of-bamboo))))
+  (testing "That the Green Dragon is an honor"
+    (is (honor? green-dragon)))
+  (testing "That the North Wind is an honor"
+    (is (honor? north-wind)))
+  )
