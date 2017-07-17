59b7e722d6e7192038d6ef3b6f7f8d262f40024e
James Laverack <james@jameslaverack.com>
2017-07-17 14:17:57 +0100

    Add pon detection test

--- test/mahjong/core_test.clj
+++ test/mahjong/core_test.clj
@@ -67,4 +67,4 @@
 (deftest pon-test
   (testing "That a pon is detected"
     (let [pon-hand [north-wind east-wind east-wind west-wind west-wind west-wind five-of-characters five-of-characters five-of-characters red-dragon white-dragon one-of-bamboo two-of-bamboo three-of-bamboo]]
-      (is (= (pons pon-hand) [[3 4 5] [6 7 8]])))))
+      (is (= (pons pon-hand) [[[west-wind west-wind west-wind] [five-of-characters five-of-characters five-of-characters]] [north-wind east-wind east-wind red-dragon white-dragon one-of-bamboo two-of-bamboo three-of-bamboo]])))))
