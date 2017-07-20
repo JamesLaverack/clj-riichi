088ac490adbdb55d161e402c98dbf1fe627d5b00
James Laverack <james@jameslaverack.com>
2017-07-21 00:37:05 +0100

    Add pon detection method

--- src/mahjong/hand.clj
+++ src/mahjong/hand.clj
@@ -2,5 +2,23 @@
   (:require [mahjong.tile]))
 
 (defn pons
-  [hand]
-  [])
+  ([hand]
+   (pons (first hand) (rest hand) [] []))
+  ([tile hand found-melds unmatched-tiles]
+    ; This is awful debugging
+    ;(println "~~~")
+    ;(println (str tile))
+    ;(println (str hand))
+    ;(println (str found-melds))
+    ;(println (str unmatched-tiles))
+   (if (empty? tile)
+     (list found-melds unmatched-tiles)
+     (if (= (get (frequencies unmatched-tiles) tile) 2)
+       ; Found a pon, remove the matching tiles from the unmatched tiles, and add the pon set to the found melds
+       (pons (first hand) (rest hand) (conj found-melds (vec (repeat 3 tile))) (vec (remove #(= tile %) unmatched-tiles)))
+       ; Didn't find a pon, add this tile to the unmatched tiles
+       (pons (first hand) (rest hand) found-melds (conj unmatched-tiles tile))
+       )
+     )
+    )
+  )
