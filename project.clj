6f7ffac12427168ae01cb805d846136ec6d406fe
James Laverack <james@jameslaverack.com>
2017-07-21 01:02:30 +0100

    Update licence to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject mahjong "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot mahjong.core
   :target-path "target/%s"
