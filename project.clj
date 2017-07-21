99a94b7a0bbf48405acf6d5e38f970171e375e8b
James Laverack <james@jameslaverack.com>
2017-07-21 01:06:02 +0100

    Remove URL and add description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject mahjong "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Riichi Mahjong simulator"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
