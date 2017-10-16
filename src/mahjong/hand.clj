d8824a2001a3a9f2a37cfa4ce4e94cb75d3ca96b
James Laverack <james@jameslaverack.com>
2017-10-16 23:11:39 +0100

    Detect chis and pons in hand validity detection

--- src/mahjong/hand.clj
+++ src/mahjong/hand.clj
@@ -1,8 +1,12 @@
 (ns mahjong.hand
   (:require [mahjong.meld]))
 
+(defn unmatched-tiles
+  [grouped-hand]
+  (nth grouped-hand 1))
+
 (defn valid?
   [hand]
   (and
     (= 14 (count hand))
-    (mahjong.meld/pair? (nth (mahjong.meld/pons hand) 1))))
+    (mahjong.meld/pair? (unmatched-tiles (mahjong.meld/chis (unmatched-tiles (mahjong.meld/pons hand)))))))
--- test/mahjong/hand_test.clj
+++ test/mahjong/hand_test.clj
@@ -7,6 +7,9 @@
   (testing "That an invalid hand is detected"
     (let [hand [red-dragon red-dragon four-of-coins three-of-bamboo west-wind east-wind green-dragon one-of-bamboo seven-of-characters eight-of-characters eight-of-characters three-of-characters four-of-characters five-of-characters]]
       (is (not (valid? hand)))))
+  (testing "Boring hand"
+    (let [hand [🀅 🀅 🀉 🀊 🀋 🀖 🀖 🀖 🀂 🀂 🀂 🀙 🀚 🀛]]
+      (is (valid? hand))))
   (testing "toitoi (four pons and a pair)"
     (let [hand [red-dragon red-dragon red-dragon three-of-characters three-of-characters three-of-characters seven-of-bamboo seven-of-bamboo seven-of-bamboo west-wind west-wind west-wind two-of-bamboo two-of-bamboo]]
       (is (valid? hand)))))
