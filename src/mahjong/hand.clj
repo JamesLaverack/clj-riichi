393f8f7273ea7a649999d57c7ce299ce5945daf8
James Laverack <james@jameslaverack.com>
2017-10-18 23:08:37 +0100

    Correct indentation

--- src/mahjong/hand.clj
+++ src/mahjong/hand.clj
@@ -10,8 +10,8 @@
   [hand]
   (let [freqs (frequencies hand)]
     (and
-     (= 7 (count (keys freqs)))
-     (every? #{2} (vals freqs)))))
+      (= 7 (count (keys freqs)))
+      (every? #{2} (vals freqs)))))
 
 (defn contains-pair?
   "Does the given hand contain at least one pair?"
@@ -29,33 +29,33 @@
         true
         ;; Check the pivot key
         (if (<=
-             (get find-freq pivot 0)
-             (get search-freq pivot 0))
+              (get find-freq pivot 0)
+              (get search-freq pivot 0))
           ;; Found enough of this key, continue to search
           (recur (first remainder) (rest remainder))
           ;; Didn't find enough of this key, fail out
           false
           )
         )
-  )))
+      )))
 
 (defn thirteen-orphans?
   [hand]
   (and
-   (contains-all? hand mahjong.tile/honors)
-   (contains-all? hand mahjong.tile/terminals)
-   (contains-pair? hand)
-   ))
+    (contains-all? hand mahjong.tile/honors)
+    (contains-all? hand mahjong.tile/terminals)
+    (contains-pair? hand)
+    ))
 
 (defn standard-hand?
   [hand]
   (and
-   (= 14 (count hand))
-   (mahjong.meld/pair? (unmatched-tiles (mahjong.meld/chis (unmatched-tiles (mahjong.meld/pons hand)))))))
+    (= 14 (count hand))
+    (mahjong.meld/pair? (unmatched-tiles (mahjong.meld/chis (unmatched-tiles (mahjong.meld/pons hand)))))))
 
 (defn valid?
   [hand]
   (or
-   (standard-hand? hand)
-   (seven-sisters? hand)
-   (thirteen-orphans? hand)))
+    (standard-hand? hand)
+    (seven-sisters? hand)
+    (thirteen-orphans? hand)))
