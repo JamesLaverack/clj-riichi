86c5f285f17a6df06e8c28702ad5b856cffb93b5
James Laverack <james@jameslaverack.com>
2017-10-25 19:46:43 +0100

    Add project URL

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject riichi/riichi "0.1.0-SNAPSHOT"
   :description "Rīchi Mahjong simulator"
+  :url "https://github.com/JamesLaverack/riichi"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
