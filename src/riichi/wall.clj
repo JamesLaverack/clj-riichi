a7ba584ae05b34c6a28dfdff8f0b56d6eea658d5
James Laverack <james@jameslaverack.com>
2017-12-08 01:22:04 +1100

    Update wall code

--- src/riichi/wall.clj
+++ src/riichi/wall.clj
@@ -5,15 +5,11 @@
   []
   (shuffle tile/complete-set))
 
-(defn pick
-  [v keys]
-  (vals (select-keys v keys)))
-
 (defn create-wall
   []
   (let [wall (vec (generate-tileset))
         dead-start (- 136 14)]
-    {:wall           (seq (subvec wall 0 dead-start))
+    {:wall           (subvec wall 0 dead-start)
      :dora           (subvec wall dead-start (+ dead-start 5))
      :ura-dora       (subvec wall (+ dead-start 5) (+ dead-start 10))
-     :kan-bonus-tile (seq (subvec wall (+ dead-start 10) (+ dead-start 14)))}))
+     :kan-bonus-tile (subvec wall (+ dead-start 10) (+ dead-start 14))}))
