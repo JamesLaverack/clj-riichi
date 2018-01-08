0c6874a71f9c26a19e96751185918f6431d7d3b3
James Laverack <james@jameslaverack.com>
2018-01-09 01:48:01 +1100

    Add tile dealing method

--- src/riichi/game.clj
+++ src/riichi/game.clj
@@ -1,6 +1,13 @@
 (ns riichi.game
-  (:require [riichi.hand :as hand]))
+  (:require [riichi.hand :as hand])
+  (:require [riichi.wall :as wall]))
 
 (defn current-player
   [game]
   (nth (first (filter #(= 14 (hand/effective-size (:hand (nth % 1)))) (seq (:players game)))) 0))
+
+(defn deal
+  [game player]
+  (let
+    [f (assoc-in game [:players player :hand :hidden] (conj (:hidden (:hand (get (:players game) player))) (peek (:wall game))))]
+    (assoc-in f [:wall] (pop (:wall f)))))
--- test/riichi/game_test.clj
+++ test/riichi/game_test.clj
@@ -1,7 +1,7 @@
 (ns riichi.game-test
   (:require [clojure.test :refer :all])
   (:require [riichi.tile :refer :all])
-  (:require [riichi.game :refer [current-player]]))
+  (:require [riichi.game :refer [current-player deal]]))
 
 (deftest current-player-test
   (testing "Hand with a declared pon"
@@ -13,3 +13,15 @@
                           :north {:hand {:hidden [🀓 🀓 🀉 🀊 🀋 🀂 🀚 🀛 🀈 🀔 🀊 🀐 🀌]}}}}]
       (is (= (current-player game) :south))))
   )
+
+(deftest deal-test
+  (testing "the next tile is dealt"
+    (let [game {:wall    [🀆 🀙 🀉]
+                :players {:east  {:hand {:declared [{:taken 🀖 :from :west :provided [🀖 🀖]}]
+                                         :hidden   [🀅]}}
+                          :south {:hand {:hidden [🀉]} :pond []}
+                          :west  {:hand {:hidden [🀓]}}
+                          :north {:hand {:hidden [🀂]}}}}]
+      (let [next-game (deal game :west)]
+        (is (= [🀆 🀙] (:wall next-game)))
+        (is (= [🀓 🀉] (:hidden (:hand (:west (:players next-game))))))))))
